67140979713c9f9ba179879be783ce90e6caa680
Nicoaedhel <nicoa.cn@gmail.com>
2015-05-05 01:05:43 +0200

    Add config.ini

--- app/index.php
+++ app/index.php
@@ -16,6 +16,15 @@ $app->view->parserOptions = array(
 );
 $app->view->parserExtensions = array(new \Slim\Views\TwigExtension());
 
+// Import Site configuration
+$site_cfg = parse_ini_file('config.ini', TRUE);
+
+// Template Globals
+$twig = $app->view()->getEnvironment();
+$twig->addGlobal('SITE_NAME', $site_cfg['website']['name']);
+$twig->addGlobal('SITE_PATH', $site_cfg['website']['path']);
+$twig->addGlobal('SITE_AUTHOR', $site_cfg['website']['author']);
+
 ORM::configure(array(
     'connection_string' => 'mysql:host=localhost;dbname=my_database',
     'username' => 'database_user',
